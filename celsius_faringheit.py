c2fd8150e1d16a58faad9329b9af881c70a390b2
kirill35476 <tr.kirilllll@gmail.com>
2024-10-11 08:13:24 +0300

    fahrenheit

--- celsius_faringheit.py
+++ celsius_faringheit.py
@@ -2,14 +2,15 @@ class Temperature:
 
     def __init__(self, celsius):
         self.__celsius = celsius
+        self.__fahrenheit = celsius * 9/5 + 32
 
-    def to_fahrenheit(self,fahrenheit):
-        self.fahrenheit = fahrenheit
+    def get_fahrenheit(self):
+        return self.__fahrenheit
+
 
     def get_celsius(self):
         return self.__celsius
 
 temp = Temperature(25)
-temp.to_fahrenheit = 25 * 9/5 + 32
-print(temp.to_fahrenheit)
+print(temp.get_fahrenheit())
 print(temp.get_celsius())
