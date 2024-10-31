1d5c21f1529da2a00f573f93970835d347ad01a0
kirill35476 <tr.kirilllll@gmail.com>
2024-10-31 13:29:59 +0300

    tank

--- 3/main.py
+++ 3/main.py
@@ -4,9 +4,10 @@ from tkinter import *
 KEY_W = 87
 KEY_S = 83
 KEY_A = 65
-KEY_D = 68    
+KEY_D = 68
 
 FPS = 60
+
 def ubdate():
     player.ubdate()
     check_collision()
@@ -14,8 +15,8 @@ def ubdate():
 
 def check_collision():
     if player.inersects(enemy):
-        print('stolknulis')
-        player.undo_move()#вызов метода
+        print('танки столкнулись')
+        player.undo_move()  # вызов метода
 
 def key_press(event):
     if event.keycode == KEY_W:
@@ -27,15 +28,16 @@ def key_press(event):
     if event.keycode == KEY_D:
         player.right()
     check_collision()
+
 w = Tk()
 w.title('')
 canv = Canvas(w, width=800, height=600, bg='alice blue')
 canv.pack()
 
-player = Tank(canvas=canv, x=100, y=50, ammo=100,speed=1)
+player = Tank(canvas=canv, x=100, y=50, ammo=100, speed=1)
 
 enemy = Tank(canvas=canv, x=300, y=300, ammo=100)
 
 w.bind('<KeyPress>', key_press)
-w.update()
+ubdate()
 w.mainloop()
--- 3/tank.py
+++ 3/tank.py
@@ -1,13 +1,15 @@
 from hitbox import Hitbox
 from tkinter import PhotoImage, NW
+
 class Tank:
     __count = 0
     __SIZE = 100
-    def __init__(self, canvas, x, y, model = 'Т-14 Армата', ammo=100, speed=10,
+
+    def __init__(self, canvas, x, y, model='Т-14 Армата', ammo=100, speed=10,
                  up='../img/tank_up.png',
                  down='../img/tank_down.png',
                  right='../img/tank_right.png',
-                 left='../img/tank_left.png', ):
+                 left='../img/tank_left.png',):
         self.__skin_up = PhotoImage(file=up)
         self.__skin_down = PhotoImage(file=down)
         self.__skin_left = PhotoImage(file=left)
@@ -25,7 +27,7 @@ class Tank:
         self.__vx = 0
         self.__vy = 0
         self.__speed = speed
-        self.__dx = 0 # хранение последнего перемещения танка вдоль осей x и y
+        self.__dx = 0  # хранение последнего перемещения танка вдоль осей x и y
         self.__dy = 0
         if self.__x < 0:
             self.__x = 0
@@ -52,12 +54,13 @@ class Tank:
     def left(self):
         self.__vx = -1
         self.__vy = 0
-        self.__canvas.itemconfig(self.__id,image=self.__skin_left)
+        self.__canvas.itemconfig(self.__id, image=self.__skin_left)
 
     def right(self):
         self.__vx = 1
         self.__vy = 0
-        self.__canvas.itemconfig(self.__id,image=self.__skin_right)
+        self.__canvas.itemconfig(self.__id, image=self.__skin_right)
+
     def ubdate(self):
         if self.__fuel > self.__speed:
             self.__dx = self.__vx * self.__speed
@@ -70,6 +73,7 @@ class Tank:
 
     def __create(self):
         self.__id = self.__canvas.create_image(self.__x, self.__y, image=self.__skin_up, anchor=NW)
+
     def __repaint(self):
         self.__canvas.moveto(self.__id, x=self.__x, y=self.__y)
 
@@ -81,36 +85,43 @@ class Tank:
 
     def get_x(self):
         return self.__x
+
     def get_y(self):
         return self.__y
+
     def get_ammo(self):
         return self.__ammo
+
     def get_model(self):
         return self.__model
+
     def get_hp(self):
         return self.__hp
+
     def get_xp(self):
         return self.__xp
+
     def get_fuel(self):
         return self.__fuel
+
     def get_speed(self):
         return self.__speed
 
-    def undo_move(self):# откат к предыдушим координатам
-        self.__x -= self.__dx # отмена последнего движения
+    @staticmethod
+    def get_quantity(self):
+        return self.__count
+
+    #@staticmethod
+    def get_size(self):
+        return self.__skin_up.width()
+
+    def undo_move(self):  # откат к предыдущим координатам
+        self.__x -= self.__dx  # отмена последнего движения
         self.__y -= self.__dy
         self.__fuel += self.__speed
         self.__ubdate_hitbox()
         self.__repaint()
 
-
-    @staticmethod
-    def get_quantity(self):
-        return self.__count
-    #@staticmethod
-    def get_size(self):
-        return self.__skin_up.width()
-
     def __str__(self):
-        return (f'Танк модели {self.__model}, имеет {self.__xp} опыта, имеет {self.__fuel} топливо, здоровье:{self.__hp}'
-              f', Патроны: {self.__ammo}, Координаты:({self.__x},{self.__y})')
+        return (f'Танк модели {self.__model}, имеет {self.__xp} опыта, имеет {self.__fuel}',
+                f' топливо, здоровье:{self.__hp}, Патроны: {self.__ammo}, Координаты:({self.__x},{self.__y})')
