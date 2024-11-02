2a2f836997b4bec682be2027faa4867000de3259
kirill35476 <tr.kirilllll@gmail.com>
2024-11-02 15:37:48 +0300

    tank

--- 3/main.py
+++ 3/main.py
@@ -8,21 +8,21 @@ KEY_D = 68
 
 FPS = 60
 
-def ubdate():
-    player.ubdate()
+def update():
+    player.update()
+    enemy.update()
     check_collision()
-    w.after(1000//FPS, ubdate)
+    w.after(1000 // FPS, update)
 
 def check_collision():
-    if player.inersects(enemy):
-        print('танки столкнулись')
-        player.undo_move()  # вызов метода
+    player.intersects(enemy)
+    enemy.intersects(player)
 
 def key_press(event):
     if event.keycode == KEY_W:
         player.forvard()
     if event.keycode == KEY_S:
-        player.dackward()
+        player.backward()
     if event.keycode == KEY_A:
         player.left()
     if event.keycode == KEY_D:
@@ -34,10 +34,12 @@ w.title('')
 canv = Canvas(w, width=800, height=600, bg='alice blue')
 canv.pack()
 
-player = Tank(canvas=canv, x=100, y=50, ammo=100, speed=1)
+player = Tank(canvas=canv, x=100, y=50, ammo=100, speed=1,bot = False)
 
-enemy = Tank(canvas=canv, x=300, y=300, ammo=100)
+enemy = Tank(canvas=canv, x=300, y=300, ammo=100,bot = True)
+
+enemy.set_target(player)
 
 w.bind('<KeyPress>', key_press)
-ubdate()
+update()
 w.mainloop()
